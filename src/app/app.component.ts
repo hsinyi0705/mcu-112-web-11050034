3f4caea49a42950e67dd64ac9800d7bbec4a6170
Corn <11050034@me.mcu.edu.tw>
2023-11-17 16:27:38 +0800

    feat(todo): 利用樣式類別繫結設定已完成樣式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
 
 @Component({
