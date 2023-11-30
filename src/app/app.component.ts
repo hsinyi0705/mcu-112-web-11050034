c0aa950ae993e4de1d6a9aed067c5d8fdb0f29ff
Corn <11050034@me.mcu.edu.tw>
2023-11-30 12:07:30 +0800

    feat(header): 實作待辦事項頁首元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { HeaderComponent } from './header/header.component';
 import { Component } from '@angular/core';
 import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TodoComponent],
+  imports: [HeaderComponent, TodoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
