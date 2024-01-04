92d6d4365b2e880a0c20182739d4000076e1b38a
Corn <11050034@me.mcu.edu.tw>
2024-01-04 22:42:19 +0800

    feat(todo-form): 待辦事項編輯設定表單資料

--- src/app/todo-form-page/todo-form-page.component.ts
+++ src/app/todo-form-page/todo-form-page.component.ts
@@ -1,21 +1,36 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TodoFormComponent } from '../todo-form/todo-form.component';
-import { TaskService } from '../services/task.service';
-import { Router } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs';
+
 import { Todo } from '../model/todo';
+import { TaskService } from '../services/task.service';
+import { TodoFormComponent } from '../todo-form/todo-form.component';
 
 @Component({
   selector: 'app-todo-form-page',
   standalone: true,
+  imports: [TodoFormComponent],
   templateUrl: './todo-form-page.component.html',
   styleUrl: './todo-form-page.component.css',
-  imports: [CommonModule, TodoFormComponent],
 })
-export class TodoFormPageComponent {
+export class TodoFormPageComponent implements OnInit {
   taskService = inject(TaskService);
 
+  formData?: Todo;
+
   readonly router = inject(Router);
+
+  readonly route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        filter((paramMap) => paramMap.has('id')),
+        map((paramMap) => +paramMap.get('id')!),
+        switchMap((id) => this.taskService.getById(id))
+      )
+      .subscribe((formData) => (this.formData = formData));
+  }
 
   onSave(task: Todo): void {
     this.taskService.add(task).subscribe(() => this.onCancel());
--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,5 +1,14 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, EventEmitter, HostBinding, Output } from '@angular/core';
+
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  OnChanges,
+  Output,
+} from '@angular/core';
+
 import {
   FormArray,
   FormControl,
@@ -18,9 +27,12 @@ import { Todo } from '../model/todo';
   templateUrl: './todo-form.component.html',
   styleUrl: './todo-form.component.css',
 })
-export class TodoFormComponent {
+export class TodoFormComponent implements OnChanges {
   @HostBinding('class')
   class = 'todo-form';
+
+  @Input()
+  initData?: Todo;
 
   @Output()
   readonly save = new EventEmitter<Todo>();
@@ -48,6 +60,12 @@ export class TodoFormComponent {
 
   get tags(): FormArray<FormControl<string | null>> {
     return this.form.get('tags') as FormArray<FormControl<string | null>>;
+  }
+
+  ngOnChanges(): void {
+    if (this.initData) {
+      this.form.patchValue(this.initData);
+    }
   }
 
   onAddTag(): void {
